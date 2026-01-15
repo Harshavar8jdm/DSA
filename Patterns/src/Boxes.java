d583032a267b0b57e0127e68a3dc50ec82dd9611
Harshavar8jdm <harshaganesh300@gmail.com>
2026-01-15 13:58:17 +0530

    Solved 9 Pattern Problems Today

--- Patterns/src/Boxes.java
+++ Patterns/src/Boxes.java
@@ -11,7 +11,14 @@ public class Boxes {
         //triangleCentered(90);
         //triangleCenteredReversed(10);
         //diamond(5);
-
+        //halfDiamond(6);
+        //binarySlopingTriangle(6);
+        //numberCollar(9);
+        //numIncrementTriangle(5);
+        //characterTriangle(22);
+        //characterTriangleReversed(26);
+        //singleCharacterTriangle(3);
+        //characterTriangleCentered(5);
     }
 
     static void filledBox(int n){
@@ -164,6 +171,125 @@ public class Boxes {
 
 
     }
+
+    static void halfDiamond(int n){
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = n; i>0 ; i--){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void binarySlopingTriangle(int n){
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for(int j = 0; j<=i; j++){
+                if(count % 2 == 0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+                count++;
+            }
+            System.out.println("");
+        }
+
+    }
+
+    static void numberCollar(int n){
+        int spaces = n + 2;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j);
+            }
+            for(int j = 1; j<=((2*n)-(2*i));  j++){
+                System.out.print(" ");
+            }
+
+            for(int j = i; j>0; j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+    static void numIncrementTriangle(int n){
+        int count = 1;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println("");
+        }
+    }
+
+    static void characterTriangle(int n){
+        if(n >26){
+            System.out.println("Not Possible");
+            return;
+        }
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(characters.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+
+    static void characterTriangleReversed(int n){
+        if(n >26){
+            System.out.println("Not Possible");
+            return;
+        }
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                System.out.print(characters.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+
+    static void singleCharacterTriangle(int n){
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(characters.charAt(i));
+            }
+            System.out.println();
+        }
+    }
+
+    static void characterTriangleCentered(int n){
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int plus = 0;
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < n - i - 1; j++){ //(2*i)+1
+                System.out.print(" ");
+            }
+            for(int j = 0; j < ((2*i)+1)/2 ; j++){
+                //System.out.print(characters.charAt(j));
+
+            }
+
+
+            for(int j = 0; j< n - i - 1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
 
 
 }
